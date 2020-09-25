854481ac5befaa4e06d9f59b716d214b413afcf5
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-25 13:23:41 +0530

    added

---
+++ src/main/java/com/employeewage/employeewages.java
@@ -0,0 +1,14 @@
+package com.employeewage;
+
+/**
+ * Hello world!
+ *
+ */
+public class employeewages
+
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
