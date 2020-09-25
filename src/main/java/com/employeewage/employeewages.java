46bbb8e42618cc2cb6de027f2a05a2ad59b7bec4
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-25 23:13:25 +0530

    added uc5

--- src/main/java/com/employeewage/employeewages.java
+++ src/main/java/com/employeewage/employeewages.java
@@ -11,7 +11,7 @@ public class employeewages
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;
 	public static final int EMP_RATE_HOUR=20;
-	
+	public static final int NUM_DAYS=2;
 
     public static void main( String[] args )
     {
@@ -19,7 +19,9 @@ public class employeewages
     	
     	int empHrs=0;
     	int empWage=0;
+    	int totalWage=0;
     	
+    	for(int day=0; day<NUM_DAYS; day++) {
     	int empCheck =(int) Math.floor(Math.random() *10)%3;
     	switch(empCheck) {
         case IS_FULL_TIME:
@@ -33,4 +35,7 @@ public class employeewages
     		break;
     }
     	empWage=empHrs*EMP_RATE_HOUR;
-    	System.out.println("Emp Wage: " +empWage);}}
+    	totalWage += empWage;
+    	System.out.println("Emp Wage: " +empWage);
+    } 
+	System.out.println("Total Emp Wage: " +totalWage);}}
