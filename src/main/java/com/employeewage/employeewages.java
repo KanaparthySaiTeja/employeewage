ba71d428d758d33474112425b38e08b47959ba6a
saiteja-code <71621072+saiteja-code@users.noreply.github.com>
2020-09-25 23:18:50 +0530

    Update employeewages.java

--- src/main/java/com/employeewage/employeewages.java
+++ src/main/java/com/employeewage/employeewages.java
@@ -7,16 +7,40 @@ package com.employeewage;
 public class employeewages
 
 {
+   public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;
+	public static final int EMP_RATE_HOUR=20;
+	public static final int NUM_DAYS=2;
+	public static final int MAX_HRS=10;
+	
     public static void main( String[] args )
     {
-    	int IS_FULL_TIME=1;
-        double empCheck = Math.floor(Math.random() * 10)%2;
-		
-    			
-    	if(empCheck == IS_FULL_TIME) {
-    	System.out.println("Employee present");
-    	} else {
-    	System.out.println("Employee absent");
-    	}
+    
+        System.out.println("Welcome to employee wage computation");
+    	
+    	int empHrs=0;
+    	int totalHrs=0;
+    	int totalWage=0;
+    	int totalWorkingDays=0;
+    	
+    	while(totalHrs<=MAX_HRS && totalWorkingDays<NUM_DAYS) {
+    		totalWorkingDays++;
+    	int empCheck =(int) Math.floor(Math.random() *10)%3;
+    	switch(empCheck) {
+        case IS_FULL_TIME:
+    		empHrs=8;
+    		break;
+        case IS_PART_TIME:
+    		empHrs=4;
+    		break;
+    	default :
+    		empHrs=0;
+    		break;
+    }
+    	totalHrs += empHrs;
+    	System.out.println("Days: " +totalWorkingDays+ " Emp hr:" +empHrs);
+    } 
+    	totalWage=totalHrs*EMP_RATE_HOUR;
+	System.out.println("Total Emp Wage: " +totalWage);
     }
 }
