b5623246d76c14a552c6ad9a8f03b2bb01892fbe
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-25 23:01:42 +0530

    added uc2

--- src/main/java/com/employeewage/employeewages.java
+++ src/main/java/com/employeewage/employeewages.java
@@ -9,14 +9,16 @@ public class employeewages
 {
     public static void main( String[] args )
     {
-    	  {
-    	    	int IS_FULL_TIME=1;
-    	        double empCheck = Math.floor(Math.random() * 10)%2;
-    			
-    	    			
-    	    	if(empCheck == IS_FULL_TIME) {
-    	    	System.out.println("Employee present");
-    	    	} else {
-    	    	System.out.println("Employee absent");
-    	    	}
-    	  }}}
+    	int IS_FULL_TIME=1;
+    	int EMP_RATE_HOUR=20;
+    	
+    	int empHrs=0;
+    	int empWage=0;
+    	
+    	double empCheck =Math.floor(Math.random() *10)%2;
+    	if(empCheck==IS_FULL_TIME)
+    		empHrs=8;
+    	else
+    		empHrs=0;
+    	empWage=empHrs*EMP_RATE_HOUR;
+    	System.out.println("Emp Wage: " +empWage);}}
