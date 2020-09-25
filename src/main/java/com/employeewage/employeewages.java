9f637b0dd65cbba2c5540bf13dbb9abc9a367651
saiteja-code <saitejakanaparthy@gmail.com>
2020-09-25 22:42:01 +0530

    added

--- src/main/java/com/employeewage/employeewages.java
+++ src/main/java/com/employeewage/employeewages.java
@@ -9,7 +9,14 @@ public class employeewages
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-        System.out.println("hello");
+    	int IS_FULL_TIME=1;
+        double empCheck = Math.floor(Math.random() * 10)%2;
+		
+    			
+    	if(empCheck == IS_FULL_TIME) {
+    	System.out.println("Employee present");
+    	} else {
+    	System.out.println("Employee absent");
+    	}
     }
 }
